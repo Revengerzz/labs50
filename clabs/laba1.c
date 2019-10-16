fcb84122dc3ef57d91c369fb105c710707ad9db4
7sug <vip.stepanchev.vip@gmail.com>
2019-10-16 07:16:42 +0000

    Добавлена лаба на Си

--- clabs/laba1.c
+++ clabs/laba1.c
@@ -4,25 +4,30 @@
 
 int main(void)
 {
-float a,x,g,y,f,bukva;
+float a,x,g,y,f;
+int bukva;
 double pi = 3.1415926;
 printf("Введите х ");
 scanf("%f", &x);
 printf("Введите a ");
 scanf("%f", &a);
-printf("Что считаем? ");
-scanf("%f", &bukva);
-if (bukva==1){
-    g=(56*(a*a)-488*a*x+320*(x*x))/(18*(a*a)-11*a*x+x*x);
-    printf("g=%f\n\n",g);
-}
-else if (bukva==2){
-    f=-1/(sin(27*a*a+12*a*x-20*x*x-pi/2));
-    printf("f=%f\n\n",f);
-}
-else{
-    y=asin(45*a*a+46*a*x+8*x*x);
-    printf("y=%f\n\n",y);
+scanf("%i",&bukva);
+switch(bukva)
+{
+    case 1:
+        g=(56*(a*a)-488*a*x+320*(x*x))/(18*(a*a)-11*a*x+x*x);
+        printf("g=%f\n\n",g);
+        break;
+    case 2:
+        f=-1/(sin(27*a*a+12*a*x-20*x*x-pi/2));
+        printf("f=%f\n\n",f);
+        break;
+    case 3:
+        y=asin(45*a*a+46*a*x+8*x*x);
+        printf("y=%f\n\n",y);
+        break;
+    default:
+        printf("неправильный ввод.\n");
 }
 
 }
--- pylabs/laba1.py
+++ pylabs/laba1.py
@@ -9,5 +9,10 @@ elif var == 2:
     F = -1/(sin(27*(a**2)+12*a*x-20*(x**2)-(pi/2)))
     print(F)
 elif var == 3:
-    Y = asin(45*(a**2)+46*a*x+8*(x**2))
-    print(Y)
+    if a == 0.1 and x == 0.1:
+        Y = asin(45*(a**2)+46*a*x+8*(x**2))
+        print(Y)
+    else:
+        print('Введены  неверные данные')
+else:
+    print('error')
