717283246b577b181578dc4247084e86f66ab820
Vlad Stepanchev <55149963+7sug@users.noreply.github.com>
2019-12-08 11:53:49 +0300

    Update laba8.py

--- pylabs/laba8.py
+++ pylabs/laba8.py
@@ -3,8 +3,8 @@ import math
 def coordinates(kol):
     data = []
     for i in range(kol):
-        x = random.randint(0, 500)
-        y = random.randint(0, 500)
+        x = random.uniform(0, 500)
+        y = random.uniform(0, 500)
         data.append((x,y))
     return(data)
 
@@ -16,13 +16,16 @@ def kollvo(tochki, centr, radius):
     count = 0
     for i in tochki:
         a = i
-        if abs(a[0] - centr[0]) <= radius:
-            if abs(a[1] - centr[1]) <= radius:
-                count+=1
+        if math.pow(a[0] - centr[0], 2) - math.pow(a[1] - centr[1], 2) <= math.pow(radius, 2):
+            count+=1
     return(count)
-
-r = float(input('Введи радиус: '))
-point = int(input('Введи кол-во точек: '))
+while True:
+    try:
+        r = float(input('Введи радиус: '))
+        point = int(input('Введи кол-во точек: '))
+        break
+    except:
+        print('Попробуй еще разок')
 points = coordinates(point)
 print('Список точек: ', points)
 mid = centre(points)
